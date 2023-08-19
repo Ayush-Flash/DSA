7590cf04225a5eb2f90f1cb875a5cbcbc94d20b4
ayush.mishra@mtxb2b.com <ayush.mishra@mtxb2b.com>
2023-08-19 13:39:28 +0530

    Added Code For Count Unique Elements

---
+++ CountDistinctElements/Main.java
@@ -0,0 +1,25 @@
+package CountDistinctElements;
+
+import java.util.*;
+
+class Main {
+    static int getCountOfUniqueElements(int[] arr) {
+        HashSet<Integer> hs = new HashSet<>();
+        int n = arr.length;
+        for(int i = 0 ; i < n ; i++) {
+            hs.add(arr[i]);
+        }
+        return hs.size();
+    }
+    public static void main(String[] args) {
+        try(Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for(int i = 0 ; i < n ; i++) {
+                arr[i] = sc.nextInt();
+            }
+            int count = getCountOfUniqueElements(arr);
+            System.out.println(count);
+        }
+    }
+}
