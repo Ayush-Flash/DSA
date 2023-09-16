b21a164f1fc5684a4be1478f6868d69560a9d68a
ayush.mishra.dsa <ayush.mishra@dsa.com>
2023-09-16 19:56:19 +0530

    Added code for segregating integers of SLL based on Even and Odd

---
+++ SegregateEvenOddNodes/Main.java
@@ -0,0 +1,51 @@
+package SegregateEvenOddNodes;
+
+import java.util.*;
+import LList.SLL;
+import LList.SLLNode;
+
+class Main {
+    public static SLLNode segerateNodes(SLL list) {
+        SLLNode temp = list.getHead();
+        SLLNode evenEnd = null, evenHead = null, oddEnd = null, oddHead = null;
+        while(temp != null) {
+            SLLNode nextTemp = temp.getNext();
+            temp.setNext(null);
+            if(temp.getData() % 2 == 0) {
+                if(evenHead == null) {
+                    evenHead = temp;
+                } else {
+                    evenEnd.setNext(temp);
+                }
+                evenEnd = temp;
+            } else {
+                if(oddHead == null) {
+                    oddHead = temp;
+                } else {
+                    oddEnd.setNext(temp);
+                }
+                oddEnd = temp;
+            }
+            temp = nextTemp;
+        }
+        if(evenEnd != null) {
+            evenEnd.setNext(oddHead);
+            list.setHead(evenHead);
+            return evenHead;
+        } else {
+            list.setHead(oddHead);
+            return oddHead;
+        }
+    }
+    public static void main(String[] args) {
+        try(Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            SLL list = new SLL();
+            for(int i = 0 ; i < n ; i++) {
+                list.insertAtEnd(sc.nextInt());
+            }
+            segerateNodes(list);
+            list.print();
+        }
+    }
+}
